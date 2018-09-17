c872ea8438bb4a7577b2f060f35afa04529b4d9b
oa102392 <oalba@csumb.edu>
2018-09-17 13:59:45 -0700

    removed nodemon

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
+    user : 'oscar',
     password : '',
-    database : 'smart-brain'
+    database : 'uni'
   }
 });
 
@@ -24,7 +24,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send(database.users) })
+app.get('/', (req, res)=> { res.send('it is working') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
